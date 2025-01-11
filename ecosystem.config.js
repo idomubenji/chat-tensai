0bdbc7a97fc8f0d19c8ace5c64c4f793623164b1
idomubenji <benjamin.vizy@gauntletai.com>
2025-01-11 16:41:38 -0500

    fix: update PM2 to use correct Next.js start command (start:next)

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   apps: [{
     name: 'chat-genius',
+    cwd: '/home/ec2-user/chat-genius',
     script: 'npm',
-    args: 'start',
+    args: 'run start:next -- -p 3000',
     instances: 1,
     autorestart: true,
     watch: false,
